73cfa3dcf682307d6fd7053d4367bb7ee32683c7
Zhuoang Tao <zhuoang2@illinois.edu>
2024-04-30 13:59:45 -0500

    secret

--- index.js
+++ index.js
@@ -16,25 +16,6 @@ db.connect((err) => {
     if (err) throw err;
     console.log("MYSQL CONNECTED");
 });
-// const { OpenAI } = require('openai'); 
-// const openai =  new OpenAI({
-//     apiKey: "sk-proj-6SeKao6DwbNRzCo7c19TT3BlbkFJjj9QYkf0F5cWKHIjQIOT",
-// })
-// app.post('/chat', async (req, res) => {
-//     const { prompt } = req.body;
-//     try {
-//         const response = await openai.chat.completions.create({
-//             messages: [{ role: "user", content: prompt }],
-//             model: "gpt-3.5-turbo",
-//           }).then(response => {
-//             res.send(response.choices[0].message.content);
-//           }).catch(error => {
-//             console.log(error);
-//           });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// })
 app.use('/', require('./routes/pages'));
 app.use("/api", require("./controllers/auth"));
 const PORT = process.env.PORT || 5000;
